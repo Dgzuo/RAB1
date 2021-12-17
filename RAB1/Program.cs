94f7a3436531c399011f693a021b0f9720a5bfe8
Вова Судаков <shopvitiaz@rambler.ru>
2021-12-17 10:19:23 +0200

    Добавьте файлы проекта.

---
+++ RAB1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rabota1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Math.Round(Ass(0.1 * Math.PI / 180), 7));
+            double ll = Math.Atan(0.1 * Math.PI / 180);
+            Console.WriteLine(ll);
+            Console.ReadKey();
+            Console.WriteLine(Math.Atan(0.3 + Math.PI / 180));
+        }
+
+        public static double Ass(double x)
+        {
+            const double eps = 1e-15;
+            double s = 0;
+            double r = x;
+            int n = 1;
+            while (Math.Abs(r) > eps)
+            {
+                s = s + r;
+                n = n + 2;
+                r = -r * x * x / (n);
+            }
+            return s;
+        }
+
+    }
+}
---
+++ uTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(Math.Round(Math.Atan(0.1 * Math.PI / 180), 4),
+            Math.Round(rabota1.Program.Ass(0.1 * Math.PI / 180), 4));
+        }
+    }
+}
